d85e3062064b853e5038402b8a00e06e157ba7ce
1039355201 <1039355201@qq.com>
2019-03-19 15:08:14 +0800

    add city-header

---
+++ src/pages/city/City.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <city-header></city-header>
+  </div>
+</template>
+<script>
+import CityHeader from './components/Header'
+export default {
+  components:{
+    CityHeader,
+  }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
           <span class="iconfont icon-sousuo"></span>
           输入城市/景点/游玩主题
         </div>
-        <div class="header-right">
-          {{city}}
-          <span class="iconfont icon-jiantou"></span>
-        </div>
+        <router-link to="/City">
+          <div class="header-right">
+            {{city}}
+            <span class="iconfont icon-jiantou"></span>
+          </div>
+        </router-link>
         <!-- <router-link to="/list">列表页</router-link> -->
          <!-- <router-link to="/list">列表页</router-link> -->
     </div>
@@ -48,6 +50,7 @@ export default {
        width:1.24rem
        float:right
        text-align :center
+       color:#fff
        .icon-jiantou
           margin-left:-0.04rem
      .header-input
